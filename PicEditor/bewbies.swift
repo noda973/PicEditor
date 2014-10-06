00626b1ba37040c9bcba6a5dd13350b6643853d2
Peter Lee <peterlee@Peter-Lees-MacBook-Pro.local>
2014-10-05 21:09:02 -0400

    qwe

---
+++ PicEditor/bewbies.swift
@@ -0,0 +1,13 @@
+//
+//  bewbies.swift
+//  PicEditor
+//
+//  Created by Peter Lee on 10/5/14.
+//  Copyright (c) 2014 Paul Pan, Peter Lee. All rights reserved.
+//
+
+import Foundation
+
+func herro () {
+    println("b00bs")
+}
